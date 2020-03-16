4fb3960aa5ce04cc0746551e578610738064a960
kiselevgleb <kiselevgleb@gmail.com>
2020-03-16 14:04:19 +0300

    event-seach-menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,11 +5,15 @@ for (let index = 0; index < menu.length; index++) {
     let element = menu[index];
     element.onclick = function () {
         if (element.parentElement.childNodes.item(3) != null) {
-            for (let index = 0; index < menuActive.length; index++) {
-                let element = menuActive[index];
-                element.classList.remove("menu_active");
+            if (element.parentElement.childNodes.item(3).classList.contains("menu_active")) {
+                element.parentElement.childNodes.item(3).classList.remove("menu_active");
+            } else {
+                for (let index = 0; index < menuActive.length; index++) {
+                    let element = menuActive[index];
+                    element.classList.remove("menu_active");
+                }
+                element.parentElement.childNodes.item(3).classList.add("menu_active");
             }
-            element.parentElement.childNodes.item(3).classList.add("menu_active");
         }
         return false;
     }
