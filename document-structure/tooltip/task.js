50a456a2019fb1be818b18d6d085e4731f5907b0
kiselevgleb <kiselevgleb@gmail.com>
2020-03-26 18:44:29 +0300

    tooltip2

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,6 +5,10 @@ title.forEach(element => {
     element.onclick = function () {
             console.log(element.title);
             titleTool.textContent=element.title;
+            let coords = element.getBoundingClientRect();
+            titleTool.style.left = coords.left + "px";
+            titleTool.style.top = coords.bottom + "px";
+          
         titleTool.classList.add("tooltip_active");
         
     }
